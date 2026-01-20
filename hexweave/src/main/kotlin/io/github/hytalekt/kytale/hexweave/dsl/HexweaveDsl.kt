c521eb5053aa83aec60294927c5c9319326ec2a5
oglassdev <23040613+oglassdev@users.noreply.github.com>
2026-01-20 13:40:48 -0500

    refactor: delete hexweave

--- hexweave/src/main/kotlin/io/github/hytalekt/kytale/hexweave/dsl/HexweaveDsl.kt
+++
@@ -1,9 +0,0 @@
-package io.github.hytalekt.kytale.hexweave.dsl
-
-/**
- * DSL marker for Hexweave builders.
- *
- * Prevents scope pollution in nested Hexweave DSL blocks.
- */
-@DslMarker
-annotation class HexweaveDsl
