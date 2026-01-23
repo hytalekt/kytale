317f5f9950d6c38da1a284ca6a9fa12ca52f46c5
marcuscastelo <marcuscastelo@usp.br>
2026-01-23 13:43:01 -0300

    feat: register SpawnEntity and DumpContainer interactions in ExamplePlugin

--- example/src/main/kotlin/io/github/hytalekt/kytale/example/ExamplePlugin.kt
+++ example/src/main/kotlin/io/github/hytalekt/kytale/example/ExamplePlugin.kt
@@ -3,15 +3,26 @@ package io.github.hytalekt.kytale.example
 import com.hypixel.hytale.server.core.plugin.JavaPlugin
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit
 import io.github.hytalekt.kytale.example.command.*
+import io.github.hytalekt.kytale.example.interaction.simpleblock.DumpContainerInteraction
+import io.github.hytalekt.kytale.example.interaction.simpleinstant.SpawnEntityInteraction
+import io.github.hytalekt.kytale.interaction.extension.interactionRegistry
+import io.github.hytalekt.kytale.interaction.extension.register
 
 class ExamplePlugin(
     init: JavaPluginInit,
 ) : JavaPlugin(init) {
     override fun setup() {
-        commandRegistry.registerCommand(KitCommand)
-        commandRegistry.registerCommand(TeleportCommand)
-        commandRegistry.registerCommand(GamemodeCommand)
-        commandRegistry.registerCommand(GiveCommand)
-        commandRegistry.registerCommand(WorldCommand)
+        with(commandRegistry) {
+            registerCommand(KitCommand)
+            registerCommand(TeleportCommand)
+            registerCommand(GamemodeCommand)
+            registerCommand(GiveCommand)
+            registerCommand(WorldCommand)
+        }
+
+        with(interactionRegistry) {
+            register(SpawnEntityInteraction)
+            register(DumpContainerInteraction)
+        }
     }
 }
