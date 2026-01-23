c5f878dc0126c7a7ac8b7f3609c818dd5df061ed
oglassdev <23040613+oglassdev@users.noreply.github.com>
2026-01-23 00:45:41 -0500

    fix: use local Hytale server jars, then fallback to stubs

--- buildSrc/src/main/kotlin/buildlogic.hytale.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.hytale.gradle.kts
@@ -2,19 +2,45 @@ plugins {
     id("buildlogic.common")
 }
 
-val isGitHubActions = providers.environmentVariable("GITHUB_ACTIONS").isPresent
+val hytaleDependency =
+    (
+        project.findProperty("hytale_path") as String? ?: run {
+            val os =
+                org.gradle.internal.os.OperatingSystem
+                    .current()
+
+            val patchline = project.findProperty("hytale_patchline") as String? ?: "release"
+
+            when {
+                os.isWindows -> {
+                    "${System.getProperty("user.home")}/AppData/Roaming/Hytale"
+                }
+
+                os.isMacOsX -> {
+                    "${System.getProperty("user.home")}/Library/Application Support/Hytale"
+                }
+
+                os.isLinux -> {
+                    "${System.getProperty("user.home")}/.local/share/Hytale"
+                }
+
+                else -> {
+                    null
+                }
+            }?.plus("/install/$patchline/package/game/latest/Server/HytaleServer.jar")
+        }
+    )?.let {
+        val jar = File(it)
+        if (!jar.exists()) {
+            logger.warn("Local Hytale JAR not found! Falling back to stubs")
+        }
+        files(jar)
+    } ?: "io.github.hytalekt.stubs:hytale-stubs:0.1.0-alpha.0"
 
 repositories {
-    if (isGitHubActions) {
-        maven("https://stubs.oglass.dev/repository")
-    }
+    maven("https://stubs.oglass.dev/repository")
 }
 
-// TODO: replace with proper Hytale dependency when Hypixel publishes it
 dependencies {
-    if (isGitHubActions) {
-        compileOnly("io.github.hytalekt.stubs:hytale-stubs:0.1.0-alpha.0")
-    } else {
-        compileOnly(files(rootDir.resolve("libs/HytaleServer.jar")))
-    }
+    compileOnly(hytaleDependency)
 }
