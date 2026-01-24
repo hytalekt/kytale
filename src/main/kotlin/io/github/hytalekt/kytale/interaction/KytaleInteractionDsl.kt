2a57d494bc81e8599a614dd622cfdc3176027e77
marcuscastelo <marcuscastelo@usp.br>
2026-01-23 21:46:51 -0300

    feat: enhance interaction functions to use delegates and codec builders

--- src/main/kotlin/io/github/hytalekt/kytale/interaction/KytaleInteractionDsl.kt
+++ src/main/kotlin/io/github/hytalekt/kytale/interaction/KytaleInteractionDsl.kt
@@ -1,9 +1,13 @@
 package io.github.hytalekt.kytale.interaction
 
+import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction
+import com.hypixel.hytale.server.core.modules.interaction.interaction.config.client.SimpleBlockInteraction
+import io.github.hytalekt.kytale.codec.CodecBuilder
+import io.github.hytalekt.kytale.codec.newCodecBuilder
 import io.github.hytalekt.kytale.interaction.simpleblock.KytaleSimpleBlockInteractionBuilder
-import io.github.hytalekt.kytale.interaction.simpleblock.internalSimpleBlockInteraction
+import io.github.hytalekt.kytale.interaction.simpleblock.KytaleSimpleBlockInteractionDelegate
 import io.github.hytalekt.kytale.interaction.simpleinstant.KytaleSimpleInstantInteractionBuilder
-import io.github.hytalekt.kytale.interaction.simpleinstant.internalSimpleInstantInteraction
+import io.github.hytalekt.kytale.interaction.simpleinstant.KytaleSimpleInstantInteractionDelegate
 
 @DslMarker
 @Target(
@@ -16,16 +20,29 @@ import io.github.hytalekt.kytale.interaction.simpleinstant.internalSimpleInstant
 annotation class KytaleInteractionDsl
 
 inline fun simpleInstantInteraction(
-    interactionId: String,
-    block: @KytaleInteractionDsl KytaleSimpleInstantInteractionBuilder.() -> Unit
-) = internalSimpleInstantInteraction(
-    interactionId, block,
-)
+    interactionId: String, block: @KytaleInteractionDsl KytaleSimpleInstantInteractionBuilder.() -> Unit
+) = with(object : KytaleSimpleInstantInteractionDelegate() {}) {
+    KytaleSimpleInstantInteractionBuilder(
+        interactionId,
+        codecBuilder = CodecBuilder(
+            newCodecBuilder<SimpleInstantInteraction>(
+                parentCodec = SimpleInstantInteraction.CODEC,
+                supplier = { this }
+            )),
+        delegate = this
+    ).apply(block).build()
+}
 
 inline fun simpleBlockInteraction(
     interactionId: String,
     block: @KytaleInteractionDsl KytaleSimpleBlockInteractionBuilder.() -> Unit
-
-) = internalSimpleBlockInteraction(
-    interactionId, block,
-)
+) = with(object : KytaleSimpleBlockInteractionDelegate() {}) {
+    KytaleSimpleBlockInteractionBuilder(
+        interactionId = interactionId,
+        codecBuilder = CodecBuilder(newCodecBuilder<SimpleBlockInteraction>(
+            parentCodec = SimpleBlockInteraction.CODEC,
+            supplier = { this }
+        )),
+        delegate = this
+    ).apply(block).build()
+}
