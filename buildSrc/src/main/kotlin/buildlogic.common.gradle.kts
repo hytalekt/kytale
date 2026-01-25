ff5cf2a12d01bf26e69bc46e3eae15c461d3198c
oglassdev <23040613+oglassdev@users.noreply.github.com>
2026-01-24 21:39:35 -0500

    deps: use Hytale release instead of pre-release

--- buildSrc/src/main/kotlin/buildlogic.common.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.common.gradle.kts
@@ -9,7 +9,7 @@ version = "0.1.0-alpha.0"
 repositories {
     mavenCentral()
     maven("https://jitpack.io")
-    maven("https://maven.hytale.com/pre-release")
+    maven("https://maven.hytale.com/release")
 }
 
 java {
